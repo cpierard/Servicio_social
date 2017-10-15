d53bffd61d6b90c35fd292e7a87ed464aeaa27fc
Claudio Pierard <c.pierard@ciencias.unam.mx>
2017-10-15 09:52:38 -0500

    Algunos cambios en el análisis de datos

--- Strat_conv/anim_strat_conv.py
+++ Strat_conv/anim_strat_conv.py
@@ -45,6 +45,7 @@ def animar_dedalus(xm, ym, S, t, norma,  CMAP):
     p = axis.pcolormesh(xm, ym, S[0,:,:],  norm= colors.PowerNorm(gamma=norma), cmap=CMAP)
     plt.colorbar(p)
     tx = axis.set_title(str(t[0]))
+    plt.ylim(0,0.15)
 
     def init():
         print('update init')
@@ -71,7 +72,7 @@ def animar_dedalus(xm, ym, S, t, norma,  CMAP):
 
 #Abajo tienes que poner el nombre del archivo hdf5 en donde guardaste los datos.
 
-T_dat , ρ_dat, s_dat, t_dat = extraer_datos('temp_salinity_8x12pm/temp_salinity_8x12pm_s3.h5')
+T_dat , ρ_dat, s_dat, t_dat = extraer_datos('temp_salinity_8x9am/temp_salinity_8x9am_s1.h5')
 print('sim t')
 print(t_dat.shape)
 
@@ -82,7 +83,7 @@ print(t_dat.shape)
 #print(dy)
 anima_T = animar_dedalus(x, y, T_dat, t_dat, 1, 'rainbow')
 #mywriter = animation.FFMpegWriter()
-#anima_T.save('temp_salinity_7x2pm.gif',writer='imagemagick', fps=4) #nombre de como quieres que se guarde el video. 'imagemagick'
+anima_T.save('SAL_8x9am_s1.gif',writer='imagemagick', fps=8) #nombre de como quieres que se guarde el video. 'imagemagick'
 #anima_T.save('prueba2.mp4',writer=mywriter, fps=30)
 '''
 anima_ρ = animar_dedalus(x, y, ρ_dat, 'rainbow_r')
