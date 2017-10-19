03e13e0a18916a918fae587421f3057714325f6d
Claudio Pierard <c.pierard@ciencias.unam.mx>
2017-10-19 15:31:53 -0500

    modifico el run...sh

--- Strat_conv/run_mpi.sh
+++ Strat_conv/run_mpi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "**STARTING**"
 mpiexec -n 6 python3 strat_conv.py
-mpiexec -n 6 python3 merge.py ugm_28/ --cleanup
+mpiexec -n 6 python3 merge.py ugm_28_1hr/ --cleanup
 echo "**DONE**"
