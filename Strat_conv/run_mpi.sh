270a82a89d1398c6fce6e3c1cafedb3209f9bee7
Claudio Pierard <c.pierard@ciencias.unam.mx>
2017-10-19 10:04:51 -0500

    Cambio t_b a 28

--- Strat_conv/run_mpi.sh
+++ Strat_conv/run_mpi.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 echo "**STARTING**"
 mpiexec -n 6 python3 strat_conv.py
-mpiexec -n 6 python3 merge.py ugm_24/ --cleanup
-mpiexec -n 6 python3 strat_conv_30.py
-mpiexec -n 6 python3 merge.py ugm_30/ --cleanup
-mpiexec -n 6 python3 strat_conv_40.py
-mpiexec -n 6 python3 merge.py ugm_40/ --cleanup
+mpiexec -n 6 python3 merge.py ugm_28/ --cleanup
 echo "**DONE**"
